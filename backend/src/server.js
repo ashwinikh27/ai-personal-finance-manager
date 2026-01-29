514d8fc16f38e3f4b51655757674cb50ee58b6f9
ashwini27 <ashwinikh027@gmail.com>
2026-01-29 22:58:40 +0530

    Day 3: MongoDB connection and User schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,10 @@
 const app = require("./app");
+const connectDB = require("./config/db");
+require("dotenv").config();
 
-const PORT = 5000;
+connectDB();
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
