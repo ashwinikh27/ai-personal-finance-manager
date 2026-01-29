99b8d9adc9551b138b26ba1945bd9f554771e6d3
ashwini27 <ashwinikh027@gmail.com>
2026-01-29 23:19:01 +0530

    Day 4: User registration API with password hashing

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", require("./routes/authRoutes"));
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
