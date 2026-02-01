037ae5e46311216d5455ca3cc5e58be09eb25907
ashwini27 <ashwinikh027@gmail.com>
2026-02-01 10:37:29 +0530

    Day 8: Get, delete and filter expenses

--- backend/src/controllers/expenseController.js
+++ backend/src/controllers/expenseController.js
@@ -24,4 +24,36 @@ const addExpense = async (req, res) => {
   }
 };
 
-module.exports = { addExpense };
+
+const getExpenses = async (req, res) => {
+  try {
+    const expenses = await Expense.find({ user: req.user }).sort({ date: -1 });
+    res.json(expenses);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+const deleteExpense = async (req, res) => {
+  try {
+    const expense = await Expense.findById(req.params.id);
+
+    if (!expense) {
+      return res.status(404).json({ message: "Expense not found" });
+    }
+
+    if (expense.user.toString() !== req.user) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    await expense.deleteOne();
+    res.json({ message: "Expense deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+
+module.exports = { addExpense, getExpenses, deleteExpense };
